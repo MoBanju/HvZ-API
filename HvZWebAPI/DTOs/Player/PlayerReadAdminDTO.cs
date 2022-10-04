13b9300c190ce872b8df047c190210205eb20d06
Øyvind Sande Reitan <oyvind.reitan@no.experis.com>
2022-10-04 11:54:54 +0200

    feat: added player DTO's

---
+++ HvZWebAPI/DTOs/Player/PlayerReadAdminDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace HvZWebAPI.Models
+{
+    public class PlayerReadAdminDTO
+    {
+        public int Id { get; set; }
+
+        public bool IsPatientZero { get; set; }
+        public bool IsHuman { get; set; }
+        public string BiteCode { get; set; }
+        
+      
+    }
+}
---
+++ HvZWebAPI/DTOs/Player/PlayerReadNonAdminDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace HvZWebAPI.Models
+{
+    public class PlayeReadNonAdminDTO
+    {
+        public int Id { get; set; }
+
+        public bool IsHuman { get; set; }
+        public string BiteCode { get; set; }
+      
+    }
+}
