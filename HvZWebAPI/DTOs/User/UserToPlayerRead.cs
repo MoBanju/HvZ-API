87b0bde075cd59fcf67f4dd46cbb7523e05b4a64
Øyvind Sande Reitan <oyvind.reitan@no.experis.com>
2022-10-06 15:04:36 +0200

    fix: null error

--- HvZWebAPI/DTOs/User/UserToPlayerRead.cs
+++ HvZWebAPI/DTOs/User/UserToPlayerRead.cs
@@ -10,8 +10,8 @@ public class UserToPlayerReadResolver : IValueResolver<Models.Player, PlayerRead
     public UserCreateDTO Resolve(Models.Player source, PlayerReadAdminDTO destination, UserCreateDTO destMember, ResolutionContext context)
     {
         var userDTO = new UserCreateDTO();
-        userDTO.FirstName = source.User.FirstName;
-        userDTO.LastName = source.User.LastName;
+        userDTO.FirstName = source.User.FirstName??"";
+        userDTO.LastName = source.User.LastName??"";
         userDTO.Id = source.User.Id;
         return userDTO;
     }
