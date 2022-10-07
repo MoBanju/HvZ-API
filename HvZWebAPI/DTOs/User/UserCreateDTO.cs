567ce90cc066396423783905146fb73c968e0ff1
Øyvind Sande Reitan <oyvind.reitan@no.experis.com>
2022-10-07 15:00:42 +0200

    fix: post-merge changes to make it run

--- HvZWebAPI/DTOs/User/UserCreateDTO.cs
+++ HvZWebAPI/DTOs/User/UserCreateDTO.cs
@@ -1,9 +1,7 @@
 ﻿namespace HvZWebAPI.DTOs.User;
 
-namespace HvZWebAPI.DTOs.User
+public class UserCreateDTO
 {
-    public class UserCreateDTO
-    {
     public int Id { get; set; }
     public string? FirstName { get; set; }
     public string? LastName { get; set; }
