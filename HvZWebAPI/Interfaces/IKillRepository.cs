2d7a52863f75c814bbcc322d05f8afa6e5829acb
MBanja <MBanja@N-NO-01-04-8521>
2022-10-08 20:48:33 +0200

    refactor: changed the Kill repository interface to inheret the IRepository, with ana extra function

--- HvZWebAPI/Interfaces/IKillRepository.cs
+++ HvZWebAPI/Interfaces/IKillRepository.cs
@@ -2,11 +2,8 @@ using HvZWebAPI.Models;
 
 namespace HvZWebAPI.Interfaces;
 
-public interface IKillRepository
+public interface IKillRepository : IRepository<Kill>
 {
-    public Task<Kill> GetById(int id);
-    public Task<IEnumerable<Kill>> GetAll();
-    public Task<Kill?> Add(Kill kill);
-    public Task<bool> Update(Kill kill);
-    public Task<bool> Delete(Kill kill);
+
+    public Task<IEnumerable<Kill>> GetAllByGameId(int gameId);
 }
