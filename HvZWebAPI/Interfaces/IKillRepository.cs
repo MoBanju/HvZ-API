ca5994e9e82e1fc4762aee15ec19ed273319312f
MBanja <MBanja@N-NO-01-04-8521>
2022-10-11 10:22:40 +0200

    feat: changed the whole interface and the way the repository works

--- HvZWebAPI/Interfaces/IKillRepository.cs
+++ HvZWebAPI/Interfaces/IKillRepository.cs
@@ -2,8 +2,16 @@ using HvZWebAPI.Models;
 
 namespace HvZWebAPI.Interfaces;
 
-public interface IKillRepository : IRepository<Kill>
+public interface IKillRepository //: IRepository<Kill>
 {
 
     public Task<IEnumerable<Kill>> GetAllByGameId(int gameId);
+    public Task<Kill> GetById(int gameId, int killId);
+    public Task<IEnumerable<Kill>> GetAll();
+    public Task<Kill?> Add(int gameId, Kill entity);
+    public Task Update(int gameId, Kill entity);
+    public Task Delete(int gameId, int killId);
+
+
+    // public Task<Kill> GetByGameId(int killId, int gameId);
 }
--- HvZWebAPI/Repositories/KillRepository.cs
+++ HvZWebAPI/Repositories/KillRepository.cs
@@ -1,6 +1,8 @@
 using HvZWebAPI.Data;
 using HvZWebAPI.Interfaces;
 using HvZWebAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Numerics;
 
 namespace HvZWebAPI.Repositories;
 
@@ -12,8 +14,9 @@ public class KillRepository : IKillRepository
     {
         _context = context;
     }
-    public async Task<Kill?> Add(Kill kill)
+    public async Task<Kill?> Add(int gameId, Kill kill)
     {
+        if (!GameExists(gameId)) throw new ArgumentException("Game by that id does not exsist");
         _context.Kills.Add(kill);
         int rowsAffected = await _context.SaveChangesAsync();
         if(rowsAffected == 0)
@@ -21,30 +24,104 @@ public class KillRepository : IKillRepository
         return kill;
     }
 
-    public async Task<bool> Delete(Kill kill)
+    public async Task Delete(int gameId, int killId)
     {
-        _context.Kills.Remove(kill);
-        int rowsAffected = await _context.SaveChangesAsync();
-        return rowsAffected > 0;
+        int rowsChanged = 0;
+        try
+        {
+            var kill = await FindKillInGame(gameId, killId);
+
+            _context.Kills.Remove(kill);
+
+            rowsChanged = await _context.SaveChangesAsync();
+        }
+        catch (Exception e)
+        {
+            throw;
+        }
+
     }
 
     public async Task<IEnumerable<Kill>> GetAll()
     {
-        return await _context.Kills.Includes(g => g.Game).ToListAsync();
+        return await _context.Kills.Include(g => g.Game).ToListAsync();
     }
 
     public async Task<IEnumerable<Kill>> GetAllByGameId(int gameId)
     {
-        return await _context.Kills.Includes(k => k.Game).Where(k => k.GameId == gameId).ToListAsync();
+        if (!GameExists(gameId)) throw new ArgumentException("Game by that id does not exsist");
+
+        return await _context.Kills.Include(k => k.Game).Where(k => k.GameId == gameId).ToListAsync();
     }
 
-    public Task<Kill> GetById(int id)
+    public async Task<Kill> GetById(int gameId, int killId)
     {
-        throw new NotImplementedException();
+        Kill kill = await FindKillInGame(gameId, killId);
+        return kill;
     }
 
-    public Task<bool> Update(Kill kill)
+    public async Task Update(int gameId, Kill kill)
     {
-        throw new NotImplementedException();
+        try
+        {
+            var exsistingKill = await FindKillInGame(gameId, kill.Id);
+            if (exsistingKill != null)
+            {
+                //This sets the foreign keys
+                _context.Entry(exsistingKill).State = EntityState.Detached;
+                kill.KillerId = exsistingKill.KillerId;
+                kill.VictimId = exsistingKill.VictimId;
+                kill.GameId = exsistingKill.GameId;
+            }
+
+            _context.Update(kill);
+            await _context.SaveChangesAsync();
+        }
+        catch (Exception)
+        {
+            throw;
+        }
+
     }
+
+    private async Task<Kill> FindKillInGame(int gameId, int killId)
+    {
+        if (!GameExists(gameId)) throw new ArgumentException("There is no game with that id");
+
+        Kill? kill = await _context.Kills.Include(k => k.Game).FirstOrDefaultAsync(k => k.Id == killId);
+        if (kill == null)
+        {
+            throw new ArgumentException("There is no kill with that id");
+        }
+
+        if (kill.GameId != gameId)
+        {
+            throw new ArgumentException("The kill-id you sent in is not in the game you sent in");
+        }
+
+        return kill;
+    }
+
+
+    /// <summary>
+    /// Checks if the game is tracked in the context
+    /// </summary>
+    /// <param name="gameId"></param>
+    /// <returns></returns>
+    private bool GameExists(int gameId)
+    {
+        return _context.Games.Any(e => e.Id == gameId);
+    }
+
+    /// <summary>
+    /// Checks if the player is tracked in context
+    /// </summary>
+    /// <param name="killId"></param>
+    /// <returns></returns>
+    private bool KillExists(int killId)
+    {
+        return _context.Kills.Any(e => e.Id == killId);
+    }
+
+
 }
