f693790e9ae11b0925e620ac0e0a11ca8a61f1f5
Øyvind Sande Reitan <oyvind.reitan@no.experis.com>
2022-10-17 14:37:09 +0200

    fix: messagevalid

--- HvZWebAPI/DTOs/Chat/ChatCreateDTO.cs
+++ HvZWebAPI/DTOs/Chat/ChatCreateDTO.cs
@@ -1,10 +1,13 @@
 using HvZWebAPI.Models;
-using Microsoft.Build.Framework;
+using HvZWebAPI.Utils;
+using System.ComponentModel.DataAnnotations;
 
 namespace HvZWebAPI.DTOs.Chat;
 
 public class ChatCreateDTO
 {
+    [MaxLength(FValid.CHAT_MESSAGE_MAXLENGTH)]
+    [Required]
     public string Message { get; set; }
     [Required]
     public DateTime? ChatTime { get; set; }
