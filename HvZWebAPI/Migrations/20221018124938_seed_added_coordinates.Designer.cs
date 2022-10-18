e8ee88fdfd30ba0c603c5fd7cef2d91ae7b78b4d
Øyvind Sande Reitan <oyvind.reitan@no.experis.com>
2022-10-18 14:50:17 +0200

    fix: added coordinate seeding

--- HvZWebAPI/Migrations/20221018124938_seed_added_coordinates.Designer.cs
+++ HvZWebAPI/Migrations/20221018124938_seed_added_coordinates.Designer.cs
@@ -4,6 +4,7 @@ using HvZWebAPI.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace HvZWebAPI.Migrations
 {
     [DbContext(typeof(HvZDbContext))]
-    partial class HvZDbContextModelSnapshot : ModelSnapshot
+    [Migration("20221018124938_seed_added_coordinates")]
+    partial class seed_added_coordinates
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
