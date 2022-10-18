e7f6d24a5fc427da181a2a663964bfb5413ca9a4
Øyvind Sande Reitan <oyvind.reitan@no.experis.com>
2022-10-18 09:21:38 +0200

    fix: minlength maxlength applied again

--- HvZWebAPI/DTOs/User/UserCreateDTO.cs
+++ HvZWebAPI/DTOs/User/UserCreateDTO.cs
@@ -5,7 +5,7 @@ namespace HvZWebAPI.DTOs.User;
 
 public class UserCreateDTO {
     //Required if you are an administrator, not if not
-    //[MaxLength(FValid.USER_KEYCLOAK_ID_MAXLENGTH), MinLength(FValid.USER_KEYCLOAK_ID_MINLENGTH)]
+    [MaxLength(FValid.USER_KEYCLOAK_ID_MAXLENGTH), MinLength(FValid.USER_KEYCLOAK_ID_MINLENGTH)]
     public string? KeyCloakId { get; set; }
     [MaxLength(FValid.USER_FIRSTNAME_MAXLENGTH), MinLength(FValid.USER_FIRSTNAME_MINLENGTH)]
     public string? FirstName { get; set; }
