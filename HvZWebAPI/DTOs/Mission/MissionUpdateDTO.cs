a713473fa44bd75849593f5bf9f29aa1156da255
MBanja <MBanja@N-NO-01-04-8521>
2022-10-19 11:13:47 +0200

    feat: aded Update DTO to Mission

---
+++ HvZWebAPI/DTOs/Mission/MissionUpdateDTO.cs
@@ -0,0 +1,15 @@
+﻿namespace HvZWebAPI.DTOs.Mission
+{
+    public class MissionUpdateDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool Is_human_visible { get; set; }
+        public bool Is_zombie_visible { get; set; }
+        public string Description { get; set; }
+        public DateTime Start_time { get; set; }
+        public DateTime End_time { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+    }
+}
