5faa1afd35469e2843c7f329c7bfc1a5403aa0e9
Øyvind Sande Reitan <oyvind.reitan@no.experis.com>
2022-10-19 10:29:47 +0200

    feat: missionSetupDTO

--- HvZWebAPI/DTOs/Kill/KillUpdateDTO.cs
+++ HvZWebAPI/DTOs/Kill/KillUpdateDTO.cs
@@ -9,9 +9,8 @@ public class KillUpdateDTO
     public int? Id { get; set; }
     [Required]
     public DateTime? TimeDeath { get; set; }
-    [Required]
     [MaxLength(FValid.PLAYER_BITECODE_MAXLENGTH), MinLength(FValid.PLAYER_BITECODE_MINLENGTH)]
-    public string Bitecode { get; set; }
+    public string? Bitecode { get; set; }
     public double? Latitude { get; set; }
     public double? Longitude { get; set; }
     [MaxLength(FValid.KILL_DESCRIPTION_MAXLENGTH)]
---
+++ HvZWebAPI/Profiles/MissionProfile.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using HvZWebAPI.DTOs.Game;
+using HvZWebAPI.DTOs.Mission;
+using HvZWebAPI.Models;
+
+namespace HvZWebAPI.Profiles;
+
+public class MissionProfile : Profile
+{
+
+
+    public MissionProfile()
+    {
+        CreateMap<MissionCreateDTO, Mission>();
+        CreateMap<Mission, MissionReadDTO>().ReverseMap();
+
+
+    }
+
+
+
+}
